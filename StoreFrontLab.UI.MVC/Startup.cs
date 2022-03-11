24fe18285fa267e65232f55929fe0f918dbdadd8
Connor Dunn <Connor.Dunn.IT@gmail.com>
2022-03-10 18:04:21 -0600

    added UI Layer

---
+++ StoreFrontLab.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StoreFrontLab.UI.MVC.Startup))]
+namespace StoreFrontLab.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
