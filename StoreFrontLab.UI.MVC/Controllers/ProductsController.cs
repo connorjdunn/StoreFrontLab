3b740129592f161951945db3e90fde43944e78bb
Connor Dunn <Connor.Dunn.IT@gmail.com>
2022-03-23 17:32:20 -0500

    Added Shopping cart and contact form functionality

--- StoreFrontLab.UI.MVC/Controllers/ProductsController.cs
+++ StoreFrontLab.UI.MVC/Controllers/ProductsController.cs
@@ -7,6 +7,9 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using ProjectName.DATA.EF;
+using StoreFrontLab.UI.MVC.Models;
+using PagedList; //Added for access to PagedList
+using PagedList.Mvc; //Added for access to PagedList.MVC
 
 namespace StoreFrontLab.UI.MVC.Controllers
 {
@@ -15,10 +18,11 @@ namespace StoreFrontLab.UI.MVC.Controllers
         private ClimbersClubEntities db = new ClimbersClubEntities();
 
         // GET: Products
-        public ActionResult Index()
+        public ActionResult Index(int page = 1)
         {
-            var products = db.Products.Include(p => p.Category).Include(p => p.StockStatu);
-            return View(products.ToList());
+            int pageSize = 5;
+            var products = db.Products.Include(p => p.Category).Include(p => p.StockStatu).ToList();
+            return View(products.ToPagedList(page, pageSize));
         }
 
         // GET: Products/Details/5
@@ -35,6 +39,66 @@ namespace StoreFrontLab.UI.MVC.Controllers
             }
             return View(product);
         }
+        #region Custom Add-To-Cart Functionality (Called from Details View)
+
+        public ActionResult AddToCart(int qty, int productID)
+        {
+            //Create an empty shell for the LOCAL shopping cart variable
+            Dictionary<int, CartItemViewModel> shoppingCart = null;
+
+            //Check if the Session shopping cart exists. If so, we can use it to populate the local version
+            if (Session["cart"] != null)
+            {
+                //Session shopping car exists. Put it's items in the local version, which is easier to work with
+                shoppingCart = (Dictionary<int, CartItemViewModel>)Session["cart"];
+                //We need to UNBOX the Session object to its smaller, more specific type -- Explicit Casting
+            }
+
+            else
+            {
+                //If the Session cart doesn't exist yet, we need to instantiate it to get started
+                shoppingCart = new Dictionary<int, CartItemViewModel>();
+            }//After this if/else, we now have a local cart thats ready to add things to it
+
+            //Find the product they referenced by its ID
+            Product product = db.Products.Where(b => b.ProductID == productID).FirstOrDefault();
+
+            if (product == null)
+            {
+                //If given a bad ID, return the user to some other page to try again.
+                //Alternatively, we could throw some kind of error, which we will
+                //discuss further in Module 6.
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                //If the book is valid, add the line-item to the cart
+                CartItemViewModel item = new CartItemViewModel(qty, product);
+
+                //Put the item in the local cart. If they already have that product as a 
+                //cart item, instead we will update the quantity. This is a big part
+                //of why we have the dictionary.
+                if (shoppingCart.ContainsKey(product.ProductID))
+                {
+                    shoppingCart[product.ProductID].Qty += qty;
+                }
+                else
+                {
+                    shoppingCart.Add(product.ProductID, item);
+                }
+
+                //Now update the SESSION version of the cart so we can maintain that info between requests
+                Session["cart"] = shoppingCart; //No explicit casting needed here
+            }
+
+            //Send them to View their Cart Items
+            return RedirectToAction("Index", "ShoppingCart");
+
+        }
+
+        #endregion
+
+
 
         // GET: Products/Create
         public ActionResult Create()
---
+++ StoreFrontLab.UI.MVC/Models/CartItemViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using ProjectName.DATA.EF;
+
+namespace StoreFrontLab.UI.MVC.Models
+{
+    public class CartItemViewModel
+    {
+        [Range(1, int.MaxValue)]
+        public int Qty { get; set; }
+
+        public Product Product { get; set; }
+
+        public CartItemViewModel(int qty, Product product)
+        {
+            Qty = qty;
+            Product = product;
+        }
+    }
+}
